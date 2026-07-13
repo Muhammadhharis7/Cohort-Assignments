b0f7cbe3c5f7d3075ee869cd97d35c118d2ee12f
Muhammad1haris <157294037+Muhammad1haris@users.noreply.github.com>
2026-07-13 23:07:02 +0400

    eigth problem solved

--- week-2-js/hard/todo-list.js
+++ week-2-js/hard/todo-list.js
@@ -10,8 +10,93 @@
   Once you've implemented the logic, test your code by running
 */
 
-class Todo {
+// class Todo {
+//   Todo(){
+//     this.todos = []
+//   }
+//   add(Todo){
+//     this.todos.push(Todo)
+//     return this.todos;
+//   }
+//   removeindexOfToDo(index){
+//     if(this.todo.length < 1){
+//       return "The To-do list is empty"
+//     }
+//     return this.todos.splice(index,1)
+//   }
+//   update(index,updatedToDo){
+//     if(this.todo.length < 1){
+//       return "The To-do list is empty"
+//     }
+//     this.todos.splice(index,1,updatedToDo)
+//     return this.todos
+//   }
+//   getAll(){
+//     if(this.todo.length < 1){
+//       return "The To-do list is empty"
+//     }
+//     return this.todos
+//   }
+//   get(indexOfToDo){
+//     return this.todos[indexOfToDo]
+//   }
+//   clear(){
+//     if(this.todo.length < 1){
+//       return "The To-do list is empty"
+//     }
+//     return this.todo = []
+//   }
+// }
 
+// module.exports = Todo;
+
+
+class Todo {
+  constructor() {
+    this.todos = [];
+  }
+
+  add(todo) {
+    this.todos.push(todo);
+    return this.todos;
+  }
+
+  remove(index) {
+    return this.todos.splice(index, 1);
+  }
+
+  // update(index, updatedTodo) {
+  //   this.todos.splice(index, 1, updatedTodo);
+  //   return this.todos;
+  // }
+
+  update(index, updatedTodo) {
+    if (index < 0 || index >= this.todos.length) {
+      return this.todos;
+    }
+    this.todos.splice(index, 1, updatedTodo);
+    return this.todos;
+  }
+
+  getAll() {
+    return this.todos;
+  }
+
+  // get(index) {
+  //   return this.todos[index];
+  // }
+
+  get(index) {
+    if (index < 0 || index >= this.todos.length) {
+      return null;
+    }
+    return this.todos[index];
+  }
+
+  clear() {
+    this.todos = [];
+    return this.todos;
+  }
 }
 
 module.exports = Todo;
